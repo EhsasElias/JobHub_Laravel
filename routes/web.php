818e6f315992624481ffa0f880c6a32369e6424b
EhsasElias <ehsas.1998000@gmail.com>
2022-04-04 00:45:15 +0300

    secound commit

--- routes/web.php
+++ routes/web.php
@@ -17,20 +17,4 @@ use Illuminate\Support\Facades\Route;
 */
 
 
-Route::get('/index',[JobViews::class ,'index']);
-Route::get('/aboutUS',[JobViews::class ,'aboutUS']);
-Route::get('/contactUS',[JobViews::class ,'contactUS']);
-Route::get('/edutrains',[JobViews::class ,'edutrains']);
-Route::get('/exper',[JobViews::class ,'exper']);
-Route::get('/favoritejobs',[JobViews::class ,'favoritejobs']);
-Route::get('/findjob',[JobViews::class ,'findjob']);
-Route::get('/jobDetails',[JobViews::class ,'jobDetails']);
-Route::get('/Notification',[JobViews::class ,'Notification']);
-Route::get('/ourServices',[JobViews::class ,'ourServices']);
-Route::get('/profileApplication',[JobViews::class ,'profileApplication']);
-Route::get('/profileChangePassword',[JobViews::class ,'profileChangePassword']);
-Route::get('/profileDashboard',[JobViews::class ,'profileDashboard']);
-Route::get('/profileEdit',[JobViews::class ,'profileEdit']);
-Route::get('/signIN',[JobViews::class ,'signIN']);
-Route::get('/signUP',[JobViews::class ,'signUP']);
-Route::get('/skills',[JobViews::class ,'skills']);
+
